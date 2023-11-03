ba65d1f1607a56630dbde6083d34fb0d5e88b3bb
eugene <81062608+eugene225@users.noreply.github.com>
2023-11-04 01:38:52 +0900

    feat : Product update delete 추가

--- src/test/java/com/example/gccoffee/repository/ProductJdbcRepositoryTest.java
+++ src/test/java/com/example/gccoffee/repository/ProductJdbcRepositoryTest.java
@@ -2,8 +2,10 @@ package com.example.gccoffee.repository;
 
 import com.example.gccoffee.model.Category;
 import com.example.gccoffee.model.Product;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestInstance;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.jdbc.Sql;
@@ -15,6 +17,7 @@ import static org.hamcrest.MatcherAssert.assertThat;
 
 @SpringBootTest
 @Sql(scripts = "/schema.sql", executionPhase = Sql.ExecutionPhase.BEFORE_TEST_METHOD)
+@TestInstance(TestInstance.Lifecycle.PER_CLASS)
 class ProductJdbcRepositoryTest {
 
     @Autowired
@@ -52,6 +55,27 @@ class ProductJdbcRepositoryTest {
         repository.insert(newProduct);
         var product = repository.findByCategory(newProduct.getCategory());
         assertThat(product.isEmpty(), is(false));
+    }
+
+    @Test
+    @DisplayName("상품을 수정할 수 있다.")
+    void testUpdate() {
+        repository.insert(newProduct);
+        newProduct.setProductName("updated-product");
+        repository.update(newProduct);
+
+        var product = repository.findById(newProduct.getProductId());
+        assertThat(product.isEmpty(), is(false));
+        assertThat(product.get().getProductName(), is(newProduct.getProductName()));
+    }
+
+    @Test
+    @DisplayName("상품을 전체 삭제한다")
+    void testDeleteAll() {
+        repository.insert(newProduct);
+        repository.deleteAll();
+        var all = repository.findAll();
+        assertThat(all.isEmpty(), is(true));
     }
 
 }
